dd4e729101998a80aeb6b01383a818dc5101f9d3
dkalaxdk <alax0013@gmail.com>
2021-03-25 15:47:13 +0100

    Ship domains, be done?

--- src/Dibbidut/Classes/ShipDomain.java
+++ src/Dibbidut/Classes/ShipDomain.java
@@ -2,52 +2,61 @@ package Dibbidut.Classes;
 
 import Dibbidut.Interfaces.IDomain;
 
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Path2D;
+
 
 public class ShipDomain implements IDomain {
     private double height;
     private double width;
     private final int shipLength;
     private final int shipWidth;
-    private double AdvanceDiameter;
-    private double TacticalDiameter;
-    private double RadiusFore;
-    private double RadiusAft;
-    private double RadiusStarboard;
-    private double RadiusPort;
+    private double advanceDiameter;
+    private double tacticalDiameter;
+    private double radiusFore;
+    private double radiusAft;
+    private double radiusStarboard;
+    private double radiusPort;
     private double starboardOffset;
     private double aftOffset;
-    private final String Shape;
-    private double[] Dimensions;
+    private final double[] dimensions = new double[5];
+    private double Lat;
+    private double Long;
+    private final Ellipse2D.Double ellipseDomain;
+    private Path2D.Double pentagonDomain;
 
 
-    public ShipDomain(int shipLength, int shipWidth, String shape) {
+    public ShipDomain(int shipLength, int shipWidth) {
         this.shipLength = shipLength;
         this.shipWidth = shipWidth;
-        this.Shape = shape;
+        this.ellipseDomain = new Ellipse2D.Double();
+        this.pentagonDomain = new Path2D.Double();
+
     }
 
     public double getAdvanceDiameter() {
-        return AdvanceDiameter;
+        return advanceDiameter;
     }
 
     public double getTacticalDiameter() {
-        return TacticalDiameter;
+        return tacticalDiameter;
     }
 
     public double getRadiusFore() {
-        return RadiusFore;
+        return radiusFore;
     }
 
     public double getRadiusAft() {
-        return RadiusAft;
+        return radiusAft;
     }
 
     public double getRadiusPort() {
-        return RadiusPort;
+        return radiusPort;
     }
 
     public double getRadiusStarboard() {
-        return RadiusStarboard;
+        return radiusStarboard;
     }
 
     public double getHeight() {
@@ -66,43 +75,75 @@ public class ShipDomain implements IDomain {
         return aftOffset;
     }
 
-    @Override
-    public ShipDomain Update(float SOG, float COG) {
-        calculateDiameters(SOG);
-        if (this.Shape.equals("Ellipse")) {
-            calculateRadii();
-            calculateOffsets();
+    public double[] getDimensions() {
+        return dimensions;
+    }
 
-        } else if (this.Shape.equals("Pentagon")) {
-            // Implement code here
-            calculateRadii();
-            calculateDimensions();
-        }
+    public Ellipse2D.Double getDomainAsEllipse() {
+        return ellipseDomain;
+    }
+
+    public Polygon getDomainAsPolygon() {
+        return new Polygon();
+    }
+
+    @Override
+    public ShipDomain Update(float SOG, float COG, float Lat, float Long) {
+        this.Lat = Lat;
+        this.Long = Long;
+        calculateDiameters(SOG);
+        calculateRadii();
+        calculateOffsets();
+        calculateDimensions();
+        updateEllipseDomain();
+        updatePentagonDomain();
         return this;
     }
 
     private void calculateOffsets() {
-        starboardOffset = RadiusStarboard;
-        width = RadiusPort + RadiusStarboard;
-        height = RadiusAft + RadiusFore;
-        aftOffset = RadiusAft;
+        starboardOffset = radiusStarboard;
+        width = radiusPort + radiusStarboard;
+        height = radiusAft + radiusFore;
+        aftOffset = radiusAft;
     }
 
     private void calculateDiameters(float SOG) {
-        this.AdvanceDiameter =  Math.pow(10, 0.3591 * Math.log10(SOG) +0.0952);
-        this.TacticalDiameter = Math.pow(10, 0.5441 * Math.log10(SOG) -0.0795);
+        this.advanceDiameter =  Math.pow(10, 0.3591 * Math.log10(SOG) +0.0952);
+        this.tacticalDiameter = Math.pow(10, 0.5441 * Math.log10(SOG) -0.0795);
     }
 
     private void calculateRadii() {
-        this.RadiusFore = (1+1.34 * Math.sqrt(Math.pow(AdvanceDiameter,2) + Math.pow(TacticalDiameter/2,2))) * shipLength;
-        this.RadiusAft =  (1+0.67 * Math.sqrt(Math.pow(AdvanceDiameter,2) + Math.pow(TacticalDiameter/2,2))) * shipLength;
-        this.RadiusStarboard = (0.2 + TacticalDiameter) * shipLength;
-        this.RadiusPort = (0.2 + 0.75 * TacticalDiameter) * shipLength;
+        this.radiusFore = (1 + 1.34 * Math.sqrt(Math.pow(advanceDiameter,2) + Math.pow(tacticalDiameter /2,2))) * shipLength;
+        this.radiusAft =  (1 + 0.67 * Math.sqrt(Math.pow(advanceDiameter,2) + Math.pow(tacticalDiameter /2,2))) * shipLength;
+        this.radiusStarboard = (0.2 + tacticalDiameter) * shipLength;
+        this.radiusPort = (0.2 + 0.75 * tacticalDiameter) * shipLength;
     }
 
     private void calculateDimensions(){
-        Dimensions[0] = 0.9 * RadiusStarboard;
-        Dimensions[1] = -0.9 ;
+        dimensions[0] = 0.9 * radiusStarboard;
+        dimensions[1] = -0.9 * radiusPort;
+        dimensions[2] = -0.9 * radiusAft;
+        dimensions[3] = 0.75 * radiusFore - 0.25 * radiusAft;
+        dimensions[4] = 1.1 * radiusFore;
     }
+
+    private void updateEllipseDomain() {
+
+        // Updating the ellipseDomain
+        this.ellipseDomain.x = Long;
+        this.ellipseDomain.y = Lat;
+        this.ellipseDomain.width = width;
+        this.ellipseDomain.height = height;
+    }
+    private void updatePentagonDomain() {
+        this.pentagonDomain = new Path2D.Double();
+        pentagonDomain.moveTo(Lat + dimensions[1],Long - dimensions[4]);
+        pentagonDomain.lineTo(Lat + dimensions[1],Long + dimensions[3]);
+        pentagonDomain.lineTo(Lat + dimensions[2],Long);
+        pentagonDomain.lineTo(Lat - dimensions[0],Long + dimensions[3]);
+        pentagonDomain.lineTo(Lat - dimensions[0], Long - dimensions[4]);
+        pentagonDomain.closePath();
+    }
+
 
 }
