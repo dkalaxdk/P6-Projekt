a282880537441a19854d672547ac8b47b05af154
Ane Jørgensen <anesoejoe@gmail.com>
2021-03-25 15:48:14 +0100

    Removed test that was not ready

--- test/SystemTest/UpdateShipListTest.java
+++ test/SystemTest/UpdateShipListTest.java
@@ -9,16 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UpdateShipListTest {
 
-    @Test
-    public void emptyListNonEmptyBuffer_shouldAddElementToList() {
-
-        CASystem system = new CASystem();
-        AISBuffer buffer = new AISBuffer();
-        AISData data = new AISData(0);
-        buffer.Push(data);
-
-        system.UpdateShipList();
-
-        assertEquals(system.shipsInRange.get(0).mmsi, data.mmsi);
-    }
+//    @Test
+//    public void emptyListNonEmptyBuffer_shouldAddElementToList() {
+//
+//        CASystem system = new CASystem();
+//        AISBuffer buffer = new AISBuffer();
+//        AISData data = new AISData(0);
+//        buffer.Push(data);
+//
+//        system.UpdateShipList();
+//
+//        assertEquals(system.shipsInRange.get(0).mmsi, data.mmsi);
+//    }
 }
