f5a877bf3ff11606395c73b56694824554aeaa58
Jakob Sønderby Kristensen <jakob.soenderby@hotmail.com>
2021-04-13 08:47:21 +0200

    Added ShapeBorder class with method to get points along border of shape

--- src/Dibbidut/Classes/VelocityObstacle.java
+++ src/Dibbidut/Classes/VelocityObstacle.java
@@ -5,6 +5,7 @@ import math.geom2d.Vector2D;
 
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 public class VelocityObstacle implements IVelocityObstacle {
     public VO velocities;
@@ -36,7 +37,7 @@ public class VelocityObstacle implements IVelocityObstacle {
 
         for(double i = 1; i <= timeframe; i = i + 0.1) {
             Vector2D centerCollision = divideVectorByScalar(displacement, i);
-            //TODO Create a Velocity Obstacle that ensures that neither ships domain is violated
+            //TODO Ensures that neither ship domain is violated
 
             //FIXME This approach will likely only work with domains that do not change
             // As it assumes that the point the domain is drawn from keeps the same displacement
@@ -46,8 +47,6 @@ public class VelocityObstacle implements IVelocityObstacle {
             Vector2D translationVec = Displacement(obstacle.position, centerCollision);
             AffineTransform translation = new AffineTransform();
             translation.translate(translationVec.x(), translationVec.y());
-            // Area scaledDomain = obstacle.domain.GetScaledDomain(i);
-            //Area conflictArea = new Area(obstacle.domain.getDomain()).createTransformedArea(translation);
 
             Area scaledDomain = new Area(obstacle.domain.getScaledShipDomain((float)i));
             Area conflictArea = scaledDomain.createTransformedArea(translation);
