a1be33d2f980e2db41f18a4f155410a077ca127e
Jakob Sønderby Kristensen <jakob.soenderby@hotmail.com>
2021-04-20 10:11:49 +0200

    Implemented Geometry

--- src/Dibbidut/Classes/Geometry/Geometry.java
+++ src/Dibbidut/Classes/Geometry/Geometry.java
@@ -1,5 +1,25 @@
 package Dibbidut.Classes.Geometry;
 
-public class Geometry {
+public abstract class Geometry {
+    public abstract void transform(Transformation transformation);
 
+    public abstract boolean contains(Vector point);
+
+    public void rotate(double degrees) {
+        Transformation t = new Transformation();
+        t.rotate(degrees);
+        transform(t);
+    }
+
+    public void scale(double width, double height) {
+        Transformation t = new Transformation();
+        t.scale(width, height);
+        transform(t);
+    }
+
+    public void translate(double x, double y) {
+        Transformation t = new Transformation();
+        t.translate(x, y);
+        transform(t);
+    }
 }
