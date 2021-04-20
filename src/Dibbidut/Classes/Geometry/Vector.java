02e868b66b67efd039c494eeadcb6b60d07e4b48
dkalaxdk <alax0013@gmail.com>
2021-04-20 11:45:36 +0200

    Intitial Vector commit.
    Most functionality should be added.

--- src/Dibbidut/Classes/Geometry/Vector.java
+++ src/Dibbidut/Classes/Geometry/Vector.java
@@ -1,4 +1,50 @@
 package Dibbidut.Classes.Geometry;
 
 public class Vector {
+
+    double x;
+    double y;
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double dotProduct(Vector vector) {
+        return vector.x * this.x + vector.y * this.y;
+    }
+
+
+    public double length() {
+        return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+    }
+
+    public double angle(Vector vector) {
+        return Math.acos(dotProduct(vector)/(this.length() * vector.length()));
+    }
+
+    public void scaleProduct(double scalar) {
+        this.x = this.x * scalar;
+        this.y = this.y * scalar;
+    }
+
+    public void divideProduct(double scalar) {
+        this.x = this.x/ scalar;
+        this.y = this.y/ scalar;
+    }
+
+    public Vector addVector(Vector vector) {
+        return new Vector(vector.x + this.x, vector.y+ this.y);
+    }
+
+    public Vector subtractVector(Vector vector) {return new Vector(this.x - vector.x, this.y - vector.y);}
+
+
 }
--- test/Geometry/VectorTest.java
+++ test/Geometry/VectorTest.java
@@ -1,71 +1,157 @@
 package Geometry;
 
+import Dibbidut.Classes.Geometry.Vector;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class VectorTest {
-    @Test
-    public void crossProduct_Returns_Number() {
+    Vector vector;
 
+    @BeforeEach
+    public void setUp() {
+        vector = new Vector(5,5);
     }
 
-    @Test
-    public void dotProduct() {
+    @Nested
+    public class Products {
+        //TODO Evt slet det udkommenterede.
+        /*        @Test
+        public void crossProduct_returns_Number() {
+            Vector testVector = new Vector(2,2);
+            assertNotEquals(vector.crossProduct(testVector),0);
+        }
 
+        @Test
+        public void crossProduct_returns_correct_Number() {
+            Vector testVector = new Vector(2,2);
+            assertEquals(vector.crossProduct(testVector),18.258905014552557);
+        }*/
+
+        @Test
+        public void dotProduct_returns_Number() {
+            Vector testVector = new Vector(3,3);
+            assertNotEquals(vector.dotProduct(testVector),0);
+        }
+
+        @Test
+        public void dotProduct_returns_Number_correct_value() {
+            Vector testVector = new Vector(3,3);
+            assertEquals(vector.dotProduct(testVector),30);
+        }
     }
 
-    @Test
-    public void scaleProduct() {
 
+    @Nested
+    public class Transformations {
+        @Test
+        public void scaleProduct_length_is_scaled() {
+            double startingValue = vector.length();
+            // Act
+            vector.scaleProduct(2);
+            assertEquals(startingValue*2,vector.length());
+        }
+
+        @Test
+        public void scaleProduct_x_is_scaled() {
+            double startingValue = vector.getX();
+            // Act
+            vector.scaleProduct(2);
+            assertEquals(startingValue*2,vector.getX());
+        }
+
+        @Test
+        public void scaleProduct_y_is_scaled() {
+            double startingValue = vector.getY();
+            // Act
+            vector.scaleProduct(2);
+            assertEquals(startingValue*2,vector.getY());
+        }
     }
 
-    @Test
-    public void getY() {
+    @Nested
+    public class Operators {
+        @Test
+        public void addVector_returns_correct_vector_x_correct() {
+            Vector testVector = new Vector(2,2);
 
+            //Act
+            Vector resultVector = vector.addVector(testVector);
+
+            // Assert
+            assertEquals(resultVector.getX(),7);
+        }
+
+        @Test
+        public void addVector_returns_correct_vector_y_correct() {
+            Vector testVector = new Vector(2,2);
+
+            //Act
+            Vector resultVector = vector.addVector(testVector);
+
+            // Assert
+            assertEquals(resultVector.getY(),7);
+        }
+
+        @Test
+        public void subtractVector_returns_correct_vector_y_correct() {
+            Vector testVector = new Vector(8,8);
+
+            // Act
+            Vector resultVector = vector.subtractVector(testVector);
+
+            assertEquals(resultVector.getX(),5-8);
+        }
+
+        @Test
+        public void subtractVector_returns_correct_vector_x_correct() {
+            Vector testVector = new Vector(8,8);
+
+            // Act
+            Vector resultVector = vector.subtractVector(testVector);
+
+            assertEquals(resultVector.getX(),5-8);
+        }
+
+
+        @Test
+        public void divideScalar_x_correct() {
+            double scalar = 5;
+            // Act
+            vector.divideProduct(scalar);
+
+            assertEquals(vector.getX(),1);
+        }
+
+        @Test
+        public void divideScalar_y_correct() {
+            double scalar = 5;
+            // Act
+            vector.divideProduct(scalar);
+
+            assertEquals(vector.getY(),1);
+        }
     }
 
-    @Test
-    public void getX() {
+    @Nested
+    public class VectorInformation {
+        @Test
+        public void getMagnitude_returns_number(){
+            assertNotEquals(vector.length(),0);
+        }
 
-    }
+        @Test
+        public void getMagnitude_returns_number_is_correct(){
+            assertEquals(vector.length(),7.0710678118654755);
+        }
 
-    @Test
-    public void addVector() {
-
-    }
-
-    @Test
-    public void addScalar() {
-
-    }
-
-    @Test
-    public void subtractVector() {
-
-    }
-
-    @Test
-    public void subtractScalar() {
-
-    }
-
-    @Test
-    public void multiplyVector() {
-
-    }
-
-    @Test
-    public void multiplyScalar() {
-
-    }
-
-    @Test
-    public void divideVector() {
-
-    }
-
-    @Test
-    public void divideScalar() {
-
+        @Test
+        public void angle_returns_correct_number(){
+            Vector testVector = new Vector(3,3);
+            assertEquals(vector.angle(testVector),0);
+        }
     }
 }
