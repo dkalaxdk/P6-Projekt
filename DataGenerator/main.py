4fe09e3b050cb0beb09da3d14cf3f184acb15d34
dkalaxdk <alax0013@gmail.com>
2021-04-30 13:49:08 +0200

    MIght've done a faster version.

--- DataGenerator/main.py
+++ DataGenerator/main.py
@@ -44,11 +44,7 @@ def read_file(time_in_minutes):
         all_lines = []
         uv = unit_vector(COG)
         for i in range(1, time_in_minutes * 60):
-            current_line = pd.DataFrame(
-                columns=["Timestamp", "Type of mobile", "MMSI", "Latitude", "Longitude", "Navigational status", "ROT",
-                         "SOG", "COG", "Heading", "IMO", "Callsign", "Name", "Ship" "type", "Cargo type", "Width",
-                         "Length", "Type of position fixing device", "Draught", "Destination", "ETA",
-                         "Data source type", "A", "B", "C", "D"])
+            current_line = pd.DataFrame()
             # 0.515660452624 is a constant, which makes SOG result in the correct knots.
             calculatedX = calculatedX + uv[0] * (SOG * 0.515660452624)
             calculatedY = calculatedY + uv[1] * (SOG * 0.515660452624)
@@ -62,8 +58,7 @@ def read_file(time_in_minutes):
             current_line["SOG"] = [SOG]
             current_line["Length"] = [Length]
             current_line["Width"] = [Width]
-            all_lines.append(current_line)
-        output = output.append(all_lines)
+            output = output.append(current_line)
         output = output.sort_values(by=["Timestamp"], ascending=True)
 
     output.to_csv("../InputFiles/generated_file.csv", index=False)
